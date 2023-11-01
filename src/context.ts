78e26568cf6ac9ec2322fc92da7bca113fbbf299
kejun <kejun1997@gmail.com>
2023-11-01 22:50:22 +0800

    refactor: split context code

--- src/context.ts
+++ src/context.ts
@@ -1,71 +1,31 @@
-import type { Logger } from 'vite'
-import { createFilter, createLogger } from 'vite'
-
-import type { ParentNode, TemplateChildNode } from '@vue/compiler-core'
-import { builtInPlatforms, platform } from '@uni-helper/uni-env'
+import { createFilter } from 'vite'
 
 import { parse } from '@vue/compiler-dom'
 import MagicString from 'magic-string'
-import { pluginName } from './constants'
-import { getRootTemplate, hasPlatformModifier, normalizeVueProps } from './utils'
+import { getRootTemplate, transformProps } from './utils'
 
 export class Context {
-  logger: Logger
   filter: (id: unknown) => boolean
   constructor() {
-    this.logger = createLogger(undefined, {
-      prefix: pluginName,
-    })
-
     this.filter = createFilter('**/*.?(nu)vue')
   }
 
   transform(code: string, id: string) {
     const ast = parse(code)
-    const rootTemplate = getRootTemplate(ast)
-    if (!rootTemplate)
+    const templateNode = getRootTemplate(ast)
+    if (!templateNode)
       return
     const ms = new MagicString(code)
-    const replaceProps = (node: ParentNode | TemplateChildNode) => {
-      if (node.type !== 1)
-        return
 
-      const props = normalizeVueProps(node.props)
-      const groups = props.reduce((acc, cur) => {
-        acc[cur.name] = acc[cur.name] ? [...acc[cur.name], cur] : [cur]
-        return acc
-      }, {} as Record<string, typeof props[0][]>)
+    transformProps(templateNode, ms, id)
 
-      Object.keys(groups).forEach((name) => {
-        const group = groups[name]
-        const defaultProp = group.find(v => v.modifiers.length === 0 || !hasPlatformModifier(v.modifiers))
-        const platformProps = group.filter(v => hasPlatformModifier(v.modifiers) && v.modifiers.includes(platform))
-        const platformProp = platformProps[0]
-        platformProps.slice(1).forEach((v) => {
-          this.logger.warn(`${id}:${v.start.line}:${v.start.column} 属性 \`${name}\` 匹配到多个的平台修饰符，已忽略 ${v.source}`, {
-            timestamp: true,
-          })
-        })
-        const resultSource = platformProp?.source.replace(new RegExp(`\\.${builtInPlatforms.join('|\\.')}`, 'gm'), '') ?? defaultProp?.source ?? ''
-        group.forEach(v => ms.remove(v.start.offset, v.start.offset))
-
-        if (defaultProp)
-          ms.overwrite(defaultProp.start.offset, defaultProp.end.offset, resultSource)
-        else if (platformProp)
-          ms.overwrite(platformProp.start.offset, platformProp.end.offset, resultSource)
-      })
-
-      node.children.forEach((n) => {
-        replaceProps(n)
-      })
-    }
-    replaceProps(rootTemplate)
-
-    return {
-      code: ms.toString(),
-      map: ms.generateMap({
-        file: id,
-      }),
+    if (ms.hasChanged()) {
+      return {
+        code: ms.toString(),
+        map: ms.generateMap({
+          file: id,
+        }),
+      }
     }
   }
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,14 @@
-import { builtInPlatforms } from '@uni-helper/uni-env'
-import type { AttributeNode, DirectiveNode, ParentNode, RootNode } from '@vue/compiler-core'
+import { builtInPlatforms, platform } from '@uni-helper/uni-env'
+import type { AttributeNode, DirectiveNode, ParentNode, RootNode, TemplateChildNode } from '@vue/compiler-core'
+import type MagicString from 'magic-string'
+import { createLogger } from 'vite'
+import { pluginName } from './constants'
+
+export type Prop = ReturnType<typeof normalizeVueProps>[0]
+
+export const logger = createLogger(undefined, {
+  prefix: pluginName,
+})
 
 export function hasPlatformModifier(modifiers: string[]) {
   return modifiers.some(m => builtInPlatforms.includes(m as any))
@@ -27,4 +36,58 @@ export function normalizeVueProps(props: (AttributeNode | DirectiveNode)[]) {
 
 export function getRootTemplate(ast: RootNode) {
   return ast.children.find(node => node.type === 1 && node.tag === 'template') as ParentNode
+}
+
+export function groupPropsByName(props: Prop[]) {
+  return props.reduce((acc, cur) => {
+    acc[cur.name] = acc[cur.name] ? [...acc[cur.name], cur] : [cur]
+    return acc
+  }, {} as Record<string, typeof props[0][]>)
+}
+
+export function hasMultiplePlatformProps(props: Prop[], id: string) {
+  if (props.length < 1)
+    return
+  const first = props[0]
+  const last = props[props.length - 1]
+  logger.warn(`${id}:${first.start.line}:${first.start.column}-${last.end.column} 单一属性匹配到多个 ${platform} 平台的修饰符，使用 \`${first.source}\` `)
+}
+
+export function computedPropSource(platformSource = '', defaultSource: string = '') {
+  let source = defaultSource
+  if (platformSource) {
+    let [attr, ...value] = platformSource.split('=')
+    attr = attr.replace(new RegExp(`\\.${builtInPlatforms.join('|\\.')}`, 'gm'), '')
+    source = `${attr}${value.length ? '=' : ''}${value.join('=')}`
+  }
+  return source
+}
+
+export function transformProps(node: ParentNode | TemplateChildNode, ms: MagicString, id: string) {
+  if (node.type !== 1)
+    return
+
+  node.children.forEach(n => transformProps(n, ms, id))
+
+  if (!node.props.length)
+    return
+
+  const props = normalizeVueProps(node.props)
+  const groups = groupPropsByName(props)
+
+  Object.keys(groups).forEach((name) => {
+    const group = groups[name]
+    group.forEach(v => ms.remove(v.start.offset, v.end.offset))
+    const defaultProp = group.find(v => v.modifiers.length === 0 || !hasPlatformModifier(v.modifiers))
+    const platformProps = group.filter(v => hasPlatformModifier(v.modifiers) && v.modifiers.includes(platform))
+    const platformProp = platformProps[0]
+    hasMultiplePlatformProps(platformProps, id)
+
+    const resultSource = computedPropSource(platformProp?.source, defaultProp?.source)
+
+    if (defaultProp)
+      ms.overwrite(defaultProp.start.offset, defaultProp.end.offset, resultSource)
+    else if (platformProp)
+      ms.overwrite(platformProp.start.offset, platformProp.end.offset, resultSource)
+  })
 }
